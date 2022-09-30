132cede1c35f3f32e0b942713a103a544406dc75
Jonas <jonasmagnusson51@gmail.com>
2022-09-30 09:14:45 +0000

    successfully talked to server and recieved the calcProtocol

--- clientmain.cpp
+++ clientmain.cpp
@@ -22,8 +22,8 @@ int main(int argc, char *argv[]){
   int socket_desc;
   struct sockaddr_in server_addr;
   socket_desc = socket(AF_INET, SOCK_DGRAM, 0);
-  int server_struct_length = sizeof(server_addr);
-  
+  //int server_struct_length = sizeof(server_addr);
+  socklen_t server_struct_length = sizeof(server_addr);
   if(socket_desc < 0){
   	#ifdef DEBUG
   	printf("Unable to create socket\n");
@@ -34,6 +34,19 @@ int main(int argc, char *argv[]){
   else printf("Socket Created\n");
   #endif
   
+  
+  struct calcMessage{
+  	int type, message, protocol, major_version, minor_version;
+  };
+  
+  struct calcMessage calc_message;
+  calc_message.type = htons(22);
+  calc_message.message = htons(0);
+  calc_message.protocol = htons(17);
+  calc_message.major_version = htons(1);
+  calc_message.minor_version = htons(0);
+  
+  struct calcProtocol calc_protocol;
   
   //Create Socket Structure
   server_addr.sin_family = AF_INET;
@@ -54,17 +67,33 @@ int main(int argc, char *argv[]){
   else printf("Connected\n");
   #endif
   
-  //Read server message TODO this doesnt work, last argument is invalid
-  if(recvfrom(socket_desc, server_message, sizeof(server_message), 0, 
-  	(struct sockaddr*)&server_addr, (socklen_t*)sizeof(server_addr)) < 0){
-  		//Do stuff
-  
-  } else printf(server_message);
-  
-  
   //Send to
-  if(sendto(socket_desc, client_message, strlen(client_message), 0,
-  	(struct sockaddr*)&server_addr, server_struct_length) < 0){
-  		//Do stuff
+  if(sendto(socket_desc, (struct calcMessage*)&calc_message, sizeof(calcMessage), 0,
+  	(struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+  		printf("Failed to send message\n");
   	}
+  else printf("Message sent\n");
+  
+  //Read server message TODO this doesnt work, last argument is invalid
+  if(recvfrom(socket_desc, (struct calcProtocol*)&calc_protocol, sizeof(calc_protocol), 0, 
+  	(struct sockaddr*)&server_addr, &server_struct_length) < 0){
+  		//Do stuff
+		printf("Didn't work\n");
+  
+  }
+  else{
+  	printf("Type: %d \n", ntohs(calc_protocol.type));
+  	printf("Major_version: %d \n", ntohs(calc_protocol.major_version));
+  	printf("Minor_version: %d \n", ntohs(calc_protocol.minor_version));
+  	printf("ID: %u\n", ntohl(calc_protocol.id));
+  	printf("Arith: %u\n", ntohl(calc_protocol.arith));
+  	printf("Int1: %u\n", ntohl(calc_protocol.inValue1));
+  	printf("Int2: %u\n", ntohl(calc_protocol.inValue2));
+  	printf("Float1: %f\n", calc_protocol.flValue1);
+  	printf("Float2: %f\n", calc_protocol.flValue2);
+  	printf("Result: %f\n", calc_protocol.flResult);
+  }
+  
+  
+
 }
